4f18b8fd7a93c1931a69510b99434c43631f5e73
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2023-11-26 14:16:20 +0000

    feature - get dish with flavor by id

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -103,7 +103,6 @@ public class DishServiceImpl implements DishService {
 //        }
 
 
-
         //根据菜品id集合批量删除菜品数据
         //sql: delete form dish where id in (?,?,?)
         dishMapper.deleteByIds(ids);
@@ -113,7 +112,26 @@ public class DishServiceImpl implements DishService {
         dishFlavorMapper.deleteByDishIds(ids);
 
 
+    }
 
+    /**
+     * 根据id查询菜品和对应的口味数据
+     *
+     * @param id
+     * @return
+     */
+    public DishVO getByIdWithFlavor(Long id) {
+        //根据id查询菜品数据
+        Dish dish = dishMapper.getById(id);
 
+        //根据菜品id查询口味数据
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
+
+        //将查询到的数据封装VO
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish, dishVO);
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
     }
 }
