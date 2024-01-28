cdfa3ca0eeea20ebc8e64782f96552b5e0af7b80
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-01-28 21:27:18 +0000

    add a shopping cart service

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -1,23 +1,43 @@
 package com.sky.service.impl;
 
+import com.sky.context.BaseContext;
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.ShoppingCartMapper;
 import com.sky.service.ShoppingCartService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
 public class ShoppingCartServiceImpl implements ShoppingCartService {
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
     /**
      * 添加购物车
+     *
      * @param shoppingCartDTO
      */
 
     public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
         //判断当前加入购物车中的商品是否已经存在了
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
 
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         //如果已经存在了，只需要将数量加一
-
+        if (list != null && list.size() > 0) {
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber() + 1); // update shopping_cart set number = ? where id =>
+            shoppingCartMapper.updateNumberById(cart);
+        }
         //如果不存在，需要插入一条购物车数据
     }
 }
