bbe6685eeb43628a60ecf4b01e7fb20a385bfe1e
RoyLuoNanjing <115074266+RoyLuoNanjing@users.noreply.github.com>
2024-02-04 19:48:09 +0000

    update shopping cart mapper

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Update;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public interface ShoppingCartMapper {
     /**
      * 动态条件查询
+     *
      * @param shoppingCart
      * @return
      */
@@ -17,8 +19,17 @@ public interface ShoppingCartMapper {
 
     /**
      * 根据id修改商品数量
+     *
      * @param shoppingCart
      */
     @Update("update shopping_cart set number = #{number} where id = #{id}")
     void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入购物车数据
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart(name, user_id, dish_id, setmeal_id, dish_flavor, number, amount, image, create_time) " +
+            "values (#{name},#{userId},#{dishId}, #{setMealId}, #{dishFlavor}, #{number}, #{amount}, #{image}, #{createTime})")
+    void insert(ShoppingCart shoppingCart);
 }
